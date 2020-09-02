d059ed8e74e1c0e4e67f8a2ad1abe08b156f851f
nirachit <nirachit@opsta.co.th>
2020-09-02 11:34:01 +0700

    insert results to elasticsearch

--- tools/1.0.0/src/app.py
+++ tools/1.0.0/src/app.py
@@ -123,8 +123,8 @@ class Tools(AppBase):
 
     async def save_results(self, result, ref_time):
         es = Elasticsearch([{'host':'10.88.200.105','port':9200}])
-        res = es.index(index='results', doc_type='result', id=ref_time, body=result)
-        return res
+        #res = es.index(index='results', doc_type='result', id=ref_time, body=result)
+        return type(result)
 
 if __name__ == "__main__":
     asyncio.run(Tools.run(), debug=True)
